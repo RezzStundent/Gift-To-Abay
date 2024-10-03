9a3afee2d688c11058d0090229abc5ced7ed8b9d
ZaWeb <fahrizailhami8@gmail.com>
2024-10-04 00:56:58 +0700

    GiftForAbay

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementById("surpriseButton").addEventListener("click", function() {
+    const surpriseMessage = document.getElementById("surpriseMessage");
+    surpriseMessage.classList.toggle("hidden");
+});
+
